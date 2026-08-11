ab5bf5519fdea17dc9bad8505484463d248f0c3b
farooqoda <farooq@ontariodigitalacademy.org>
2026-08-11 16:32:34 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/oda_dashboard/">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
